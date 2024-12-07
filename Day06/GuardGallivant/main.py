96c944ebdf8ca355a1ad83938a7a116e031c2e61
Casper <c.d.dijkstra@hva.nl>
2024-12-07 15:18:53 +0100

    Use dictionary for turning:

--- Day06/GuardGallivant/main.py
+++ Day06/GuardGallivant/main.py
@@ -1,26 +1,66 @@
 from collections import namedtuple
 
+Coordinate = namedtuple("Coordinate", ["x", "y"])
+
 
 def process_file(filename):
     """Read the file and split into list."""
-    parsed_data = []
-    with open(filename, "r") as data:
-        for line in data.strip().split("\n"):
-            # Split the line into result and the rest using the ':' delimiter
-            result_part, numbers_part = line.split(":")
+    with open(filename, "r") as file:
+        return [line.strip() for line in file]
 
-            # Parse the result as an integer
-            result = int(result_part.strip())
 
-            # Parse the rest of the numbers as integers
-            numbers = list(map(int, numbers_part.strip().split()))
+def obtain_new_coordinates(coord, dir):
+    if dir == "N":
+        return Coordinate(coord.x - 1, coord.y)
+    elif dir == "E":
+        return Coordinate(coord.x, coord.y + 1)
+    elif dir == "S":
+        return Coordinate(coord.x + 1, coord.y)
+    elif dir == "W":
+        return Coordinate(coord.x, coord.y - 1)
 
-            # Combine result and numbers into a single list and add to the parsed data
-            parsed_data.append([result] + numbers)
-        return parsed_data
+
+def invalid_coordinates(coord, max_x, max_y):
+    return coord.x < 0 or coord.y < 0 or coord.x >= max_x or coord.y >= max_y
+
+
+def make_turn(dir):
+    return {
+        "N": "E",
+        "E": "S",
+        "S": "W",
+        "W": "N",
+    }[dir]
+
+
+def find_path(grid):
+    coordinates = next(
+        Coordinate(x, y)
+        for x, row in enumerate(grid)
+        for y, char in enumerate(row)
+        if char == "^"
+    )
+    dir = "N"
+    visited = [coordinates]
+    while True:
+        new_coordinates = obtain_new_coordinates(coordinates, dir)
+        if invalid_coordinates(new_coordinates, len(grid), len(grid[0])):
+            break
+
+        if grid[new_coordinates.x][new_coordinates.y] == "#":
+            dir = make_turn(dir)
+        else:
+            coordinates = new_coordinates
+            if coordinates not in visited:
+                visited.append(coordinates)
+
+    return len(visited)
 
 
 # Main execution
 if __name__ == "__main__":
-    equations = process_file("dummydata.txt")
-    print(equations)
+    grid = process_file("dummydata.txt")
+    assert find_path(grid) == 41
+
+    grid = process_file("data.txt")
+    print("Part 1", find_path(grid))
