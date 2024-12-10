19deb29a7666c9647c04ec1bfb7a7ffb7f187bc0
Casper <c.d.dijkstra@hva.nl>
2024-12-10 18:28:48 +0100

    greatly simplified python solution

--- Day09/DiskFragmenter/main.py
+++ Day09/DiskFragmenter/main.py
@@ -29,78 +29,27 @@ def calculate2(instructions):
         grid.extend(itertools.repeat(entry, int(instruction)))
 
     # File IDs in decreasing order
-    unique_files = sorted(set(grid) - {"."}, reverse=True)
+    unique_files = sorted(
+        set(int(value) for value in grid if value != "."), reverse=True
+    )
     file_indices = {
         file_id: Coordinate(min=min(indices), max=max(indices))
         for file_id in unique_files
-        if (indices := [idx for idx, value in enumerate(grid) if value == file_id])
+        if (indices := [idx for idx, value in enumerate(grid) if value == str(file_id)])
     }
-
-    # Find all free spans and put in array that will be updated
-    free_spans = []
-    start = None
-    for i, char in enumerate(grid):
-        if char == "." and start is None:
-            start = i
-        elif char != "." and start is not None:
-            free_spans.append(Coordinate(min=start, max=i - 1))
-            start = None
 
     # Loop through files in reversed file ID order
     for file_id, file_indices in file_indices.items():
         file_size = file_indices.max - file_indices.min + 1
-        # Find first free span that is large enough
 
-        filtered_span = next(
-            (
-                span
-                for span in free_spans
-                if span.min < file_indices.min and span.max - span.min + 1 >= file_size
-            ),
-            None,
-        )
+        for i in range(file_indices.max):
+            if all(grid[j] == "." for j in range(i, i + file_size)):
+                # Move file
+                for x in range(file_size):
+                    grid[file_indices.min + x] = "."
+                    grid[i + x] = str(file_id)
+                break
 
-        if filtered_span is None:  # Continue if not found
-            continue
-
-        # Update grid by moving file
-        for i in range(file_indices.max - file_indices.min + 1):
-            grid[filtered_span.min + i] = file_id
-            grid[file_indices.min + i] = "."
-
-        # Update free_spans; remove dots that are filled by file
-        if (
-            filtered_span.max - filtered_span.min + 1 == file_size
-        ):  # Remove if it fits exactly
-            free_spans.remove(filtered_span)
-        else:  # Otherwise update min coordinate of free span
-            free_spans[free_spans.index(filtered_span)] = Coordinate(
-                min=filtered_span.min + file_size, max=filtered_span.max
-            )
-
-        # Update free_spans; add dots where file used to be
-        lhs_span = next(
-            (span for span in free_spans if span.max == (file_indices.min - 1)), None
-        )
-        rhs_span = next(
-            (span for span in free_spans if span.min == (file_indices.max + 1)), None
-        )
-        if lhs_span and rhs_span:
-            free_spans.remove(lhs_span)
-            free_spans.remove(rhs_span)
-            free_spans.append(Coordinate(min=lhs_span.min, max=rhs_span.max))
-        elif lhs_span:
-            free_spans[free_spans.index(lhs_span)] = Coordinate(
-                min=lhs_span.min, max=file_indices.max
-            )
-        elif rhs_span:
-            free_spans[free_spans.index(rhs_span)] = Coordinate(
-                min=file_indices.min, max=rhs_span.max
-            )
-        else:  # No adjacent spans, add a new span
-            free_spans.append(Coordinate(min=file_indices.min, max=file_indices.max))
-
-        free_spans.sort(key=lambda span: span.min)
     return sum(int(grid[idx]) * idx for idx in range(len(grid)) if grid[idx] != ".")
 
 
@@ -111,5 +60,4 @@ if __name__ == "__main__":
 
     instructions = process_file("data.txt")
     print("Part 1", calculate(instructions))
-    # 6510130564589 is too high
     print("Part 2", calculate2(instructions))
