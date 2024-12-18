312b5d8573ee39669f5c804359e4c1abf8efc2b7
Casper <c.d.dijkstra@hva.nl>
2024-12-18 13:11:17 +0100

    Found solution part 2

--- Day16/ReindeerMaze/main.py
+++ Day16/ReindeerMaze/main.py
@@ -12,6 +12,13 @@ direction_map = {
     "W": (0, -1),  # West: move left
 }
 
+turns = {
+    "N": ["E", "W"],
+    "E": ["N", "S"],
+    "S": ["W", "E"],
+    "W": ["S", "N"],
+}
+
 
 def process_file(filename):
     """Read the file and split into list."""
@@ -93,71 +100,77 @@ def traverse(grid, manhatton_factor=0):
                 )
 
 
-def traverse_with_history(grid, manhatton_factor=0, error_fault=0):
+def traverse_with_history(grid):
     start_coor = find_coordinates(grid, "S")[0]
     end_coor = find_coordinates(grid, "E")[0]
+    coordinates_with_dots = [
+        Coordinate(x=row_idx, y=col_idx)
+        for row_idx, row in enumerate(grid)
+        for col_idx, value in enumerate(row)
+        if value in [".", "S", "E"]
+    ]
+    path_cost = {
+        (coord, direction): float("inf")
+        for coord in coordinates_with_dots
+        for direction in direction_map.keys()
+    }
+    path_cost[(start_coor, "E")] = 0
+
+    path_queue = []
     path = Path_History(
         Coordinate=start_coor, Cost=0, Direction="E", Visited=[start_coor]
     )
-    path_queue = []
-    min_cost = float("inf")
     total_route = []
-    other_exits = 0
+    finish_reached = False
 
-    # Use a priority queue with Cost and Manhttan factor as key
     heapq.heappush(path_queue, (0, path))
     while path_queue:
         entry = heapq.heappop(path_queue)[1]
         if entry.Coordinate == end_coor:
-            if entry.Cost <= min_cost:
-                min_cost = entry.Cost
+            if entry.Cost <= path_cost[end_coor]:
+                finish_reached = True
+                path_cost[end_coor] = entry.Cost
                 total_route.extend(entry.Visited)
-                print("Added new entry at", other_exits)
-            elif other_exits <= error_fault:
-                other_exits += 1
-                if (other_exits % 20) == 0:
-                    print(other_exits)
-            else:
-                route = list(set(tuple(c) for c in total_route))
-                return len(route)
-        directions = get_valid_directions(entry.Direction)
-        for dir, (dx, dy) in directions:
-            neighbor = Coordinate(entry.Coordinate.x + dx, entry.Coordinate.y + dy)
-            if (
-                0 <= neighbor.x < len(grid)
-                and 0 <= neighbor.y < len(grid[0])  # Within bounds
-                and grid[neighbor.x][neighbor.y] != "#"  # And can move there
-            ):
-                cost_increase = 1001 if direction_changed(entry.Direction, dir) else 1
-                new_cost = entry.Cost + cost_increase
-                new_visited = entry.Visited.copy()
-                new_visited.append(neighbor)
-                path = Path_History(neighbor, new_cost, dir, new_visited)
-                heapq.heappush(
-                    path_queue,
-                    (
-                        new_cost
-                        + manhattan_distance_factor(
-                            neighbor, end_coor, manhatton_factor
-                        ),
-                        path,
-                    ),
-                    # Add a Manhattan factor so entries closer to end are more likely
-                )
+        elif finish_reached and entry.Cost > path_cost[end_coor]:
+            route = list(set(tuple(c) for c in total_route))
+            return len(route)
+
+        # Add turns
+        for new_direction in turns[entry.Direction]:
+            new_cost = entry.Cost + 1000  # Add 1000 for turning
+            if new_cost > path_cost[(entry.Coordinate, new_direction)]:
+                continue
+
+            new_visited = entry.Visited.copy()
+            path_cost[(entry.Coordinate, new_direction)] = new_cost
+            path = Path_History(entry.Coordinate, new_cost, new_direction, new_visited)
+            heapq.heappush(path_queue, (new_cost, path))
+
+        (dx, dy) = direction_map[entry.Direction]
+        neighbor = Coordinate(entry.Coordinate.x + dx, entry.Coordinate.y + dy)
+        new_cost = entry.Cost + 1  # Add 1 for moving
+        if (
+            neighbor not in coordinates_with_dots
+            or new_cost > path_cost[(neighbor, entry.Direction)]
+        ):
+            continue
+
+        path_cost[neighbor] = new_cost
+        new_visited = entry.Visited + [neighbor]
+        path = Path_History(neighbor, new_cost, entry.Direction, new_visited)
+        heapq.heappush(path_queue, (new_cost, path))
 
 
 # Main execution
 if __name__ == "__main__":
     grid = process_file("dummydata.txt")
     assert traverse(grid) == 7036
-    # assert traverse_with_history(grid, manhatton_factor=0) == 45
+    assert traverse_with_history(grid) == 45
 
     grid = process_file("dummydata2.txt")
     assert traverse(grid) == 11048
-    # assert traverse_with_history(grid) == 64
+    assert traverse_with_history(grid) == 64
 
     grid = process_file("data.txt")
-    # print("Part 1", traverse(grid, manhatton_factor=300))
-    print(
-        "Part 2", traverse_with_history(grid, manhatton_factor=300, error_fault=500)
-    )  # 429 is too low
+    print("Part 1", traverse(grid, manhatton_factor=350))
+    print("Part 2", traverse_with_history(grid))  # 429 is too low
