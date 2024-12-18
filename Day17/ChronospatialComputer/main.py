76ec5755f20c97c00968d5375d6bb24060686471
Casper <c.d.dijkstra@hva.nl>
2024-12-18 21:32:05 +0100

    try solvin part 2

--- Day17/ChronospatialComputer/main.py
+++ Day17/ChronospatialComputer/main.py
@@ -14,16 +14,12 @@ def process_file(filename):
 
 
 def combo_operand(operand):
-    switch_dict = {
-        0: 0,
-        1: 1,
-        2: 2,
-        3: 3,
-        4: registers[0],  # Map to value of register A
-        5: registers[1],  # Map to value of register B
-        6: registers[2],  # Map to value of register C
-    }
-    return switch_dict.get(operand, "Default case")
+    if 0 <= operand <= 3:
+        return operand
+    if 4 <= operand <= 6:
+        return registers[operand - 4]
+    else:
+        raise RuntimeError("This operand should not occur", operand)
 
 
 def process_instructions(instructions, registers):
@@ -61,15 +57,44 @@ def process_instructions(instructions, registers):
 
 def calculate_A_register(instructions, registers):
     # Every time A is divided by 8 (thanks to (0,3)) and we know that it should be less than 8 in the less step.
-    all_values = [x for tup in instructions for x in tup]
-    value = 0
-    for mod in all_values[::-1]:
-        for N in range(value * 8, value * 8 + 8):
-            if N % 8 != mod:
+    possible_ranges = [range(0, 8), range(2, 4)]
+    for possible_range in possible_ranges:
+        print(possible_range)
+        for num in possible_range:
+            print(num)
+    return 1
+
+
+def find(program, ans):
+    print(program, ans)
+    if program == []:
+        return ans
+    for t in range(8):
+        a = ans << 3 | t  # This is where the magic happens
+        b = a % 8
+        b = b ^ 3
+        c = a // 2**b
+        a //= 2**3
+        b ^= a
+        b ^= c
+        if b % 8 == program[-1]:
+            print("Yup:", a)
+            sub = find(program[:-1], a)
+            if sub is None:
                 continue
-            print(range(value * 8, value * 8 + 8), mod, N)
-            value = N  # Add an exclude range due to truncation
-    return value * 8
+            return sub
+
+
+def find_test(program, ans):
+    if program == []:
+        return ans * 8
+    for t in range(8):
+        a = ans << 3 | t  # This is where the magic happens
+        if a % 8 == program[-1]:
+            sub = find_test(program[:-1], a)
+            if sub is None:
+                continue
+            return sub
 
 
 # Main execution
@@ -92,14 +117,19 @@ if __name__ == "__main__":
     assert registers[1] == 44354
 
     registers, instructions = process_file("dummydata_copy.txt")
-    assert calculate_A_register(instructions, registers) == 117440
+    all_instructions = [x for tup in instructions for x in tup]
+    print(all_instructions)
+    ans = find_test(all_instructions, 0)
+    print("Ans = ", ans)
+    assert ans == 117440
+
+    # registers, instructions = process_file("data.txt")
+    # output, registers = process_instructions(instructions, registers)
+    # print("Part 1:", output)
 
     registers, instructions = process_file("data.txt")
-    output, registers = process_instructions(instructions, registers)
-    print("Part 1:", output)
-
-    registers, instructions = process_file("data.txt")
-    output = calculate_A_register(instructions, registers)
+    all_instructions = [x for tup in instructions for x in tup]
+    output = find(all_instructions, 0)
     # 130812005036816 is too low
     # 130812005036818 is too low
     print("Part 2:", output)
