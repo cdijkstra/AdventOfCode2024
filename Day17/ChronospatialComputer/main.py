7ee2db00a4edcbfd0e4383e32266588a57bc831b
Casper <c.d.dijkstra@hva.nl>
2024-12-18 09:44:26 +0100

    Attempt to solve part 2; Made mistake that register B has to be backtracked, not A

--- Day17/ChronospatialComputer/main.py
+++ Day17/ChronospatialComputer/main.py
@@ -28,9 +28,12 @@ def combo_operand(operand):
 
 def process_instructions(instructions, registers):
     idx = 0
+
     output = []
+    print(registers)
     while idx < len(instructions):
         opcode, operand = instructions[idx]
+        print(opcode, operand)
         if opcode == 0:
             registers[0] //= 2 ** combo_operand(operand)
         elif opcode == 1:
@@ -42,15 +45,31 @@ def process_instructions(instructions, registers):
         elif opcode == 4:
             registers[1] ^= registers[2]
         elif opcode == 5:
+            print(combo_operand(operand))
             output.append(combo_operand(operand) % 8)
         elif opcode == 6:
             registers[1] = registers[0] // 2 ** combo_operand(operand)
         elif opcode == 7:
             registers[2] = registers[0] // 2 ** combo_operand(operand)
         idx += 1
+        print(registers)
 
     result = ",".join(map(str, output))
+    print(registers)
     return result, registers
+
+
+def calculate_A_register(instructions, registers):
+    # Every time A is divided by 8 (thanks to (0,3)) and we know that it should be less than 8 in the less step.
+    all_values = [x for tup in instructions for x in tup]
+    value = 0
+    for mod in all_values[::-1]:
+        for N in range(value * 8, value * 8 + 8):
+            if N % 8 != mod:
+                continue
+            print(range(value * 8, value * 8 + 8), mod, N)
+            value = N  # Add an exclude range due to truncation
+    return value * 8
 
 
 # Main execution
@@ -72,8 +91,15 @@ if __name__ == "__main__":
     output, registers = process_instructions(instructions, registers)
     assert registers[1] == 44354
 
+    registers, instructions = process_file("dummydata_copy.txt")
+    assert calculate_A_register(instructions, registers) == 117440
+
     registers, instructions = process_file("data.txt")
     output, registers = process_instructions(instructions, registers)
     print("Part 1:", output)
-    # 3,5,3,4,6,3,0,7,0 is wrong
-    # 6,2,7,6,4,3,1,5,7 is wrong
+
+    registers, instructions = process_file("data.txt")
+    output = calculate_A_register(instructions, registers)
+    # 130812005036816 is too low
+    # 130812005036818 is too low
+    print("Part 2:", output)
