dff69b2bf194a05fbd8c60bd8567a7d21d30fe9d
Casper <c.d.dijkstra@hva.nl>
2024-12-19 21:15:18 +0100

    Elegant recursive function algorithm

--- Day19/LinenLayout/main.py
+++ Day19/LinenLayout/main.py
@@ -1,6 +1,3 @@
-import re
-
-
 def process_file(filename):
     """Read the file and split into list."""
     with open(filename, "r") as file:
@@ -15,28 +12,32 @@ def can_construct(design, original_design, patterns):
     Recursive function to find out if string can be constructed from patterns
     """
     if len(design) == 0:
-        print("Possible", original_design)
         return True
 
-    print(patterns)
     for pattern in patterns:
-        print(design, pattern, design[len(pattern) :])
-        if not design.startswith(pattern):
-            continue
-        return can_construct(design[len(pattern) :], original_design, patterns)
+        if design.startswith(pattern) and can_construct(
+            design[len(pattern) :], original_design, patterns
+        ):
+            return True
 
     # If no matches work, return False
     return False
 
 
-# Main execution
-if __name__ == "__main__":
-    patterns, designs = process_file("dummydata.txt")
+def find_designs(patterns, designs):
     count = 0
     for design in designs:
         patterns_contained = [s for s in patterns if s in design]
-        print(design, patterns_contained)
         if not can_construct(design, design, patterns_contained):
             continue
         count += 1
-    print(count)
+    return count
+
+
+# Main execution
+if __name__ == "__main__":
+    patterns, designs = process_file("dummydata.txt")
+    assert find_designs(patterns, designs) == 6
+
+    patterns, designs = process_file("data.txt")
+    print("Part 1:", find_designs(patterns, designs))
