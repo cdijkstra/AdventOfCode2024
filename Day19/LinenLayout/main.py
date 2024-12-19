497f1ce216bb51ea22e0488292fd4a7cf56c5367
Casper <c.d.dijkstra@hva.nl>
2024-12-19 21:34:44 +0100

    Elegant recursive function algorithm with memoization

--- Day19/LinenLayout/main.py
+++ Day19/LinenLayout/main.py
@@ -7,7 +7,7 @@ def process_file(filename):
         return patterns, designs
 
 
-def can_construct(design, original_design, patterns):
+def can_construct(design, patterns):
     """
     Recursive function to find out if string can be constructed from patterns
     """
@@ -16,7 +16,7 @@ def can_construct(design, original_design, patterns):
 
     for pattern in patterns:
         if design.startswith(pattern) and can_construct(
-            design[len(pattern) :], original_design, patterns
+            design[len(pattern) :], patterns
         ):
             return True
 
@@ -24,20 +24,53 @@ def can_construct(design, original_design, patterns):
     return False
 
 
-def find_designs(patterns, designs):
+def construct_possibilities(design, patterns):
+    """
+    Recursive function to find out in how many ways the design can be created
+    We use memoization to ensure we don't calculate the same thing twice
+    """
+    if design in memo:
+        return memo[design]
+
+    if len(design) == 0:
+        return 1
+
     count = 0
-    for design in designs:
-        patterns_contained = [s for s in patterns if s in design]
-        if not can_construct(design, design, patterns_contained):
-            continue
-        count += 1
+    for pattern in patterns:
+        if design.startswith(pattern):
+            count += construct_possibilities(design[len(pattern) :], patterns)
+
+    memo[design] = count
+    # If no matches work, return False
+    return count
+
+
+def find_designs(patterns, designs):
+    return sum(
+        1
+        for design in designs
+        if can_construct(
+            design, [s for s in patterns if s in design]
+        )  # [s for s in patterns if s in design] gives all substring that occur in design
+    )
+
+
+def find_combinatorics(patterns, designs):
+    count = sum(
+        construct_possibilities(design, [s for s in patterns if s in design])
+        for design in designs
+    )
     return count
 
 
 # Main execution
 if __name__ == "__main__":
+    memo = {}
     patterns, designs = process_file("dummydata.txt")
     assert find_designs(patterns, designs) == 6
+    assert find_combinatorics(patterns, designs) == 16
 
     patterns, designs = process_file("data.txt")
     print("Part 1:", find_designs(patterns, designs))
+    memo = {}
+    print("Part 2:", find_combinatorics(patterns, designs))
