9c806ae059e9484a72bcb1429fffaf55fa404b6d
Casper <c.d.dijkstra@hva.nl>
2024-12-20 17:00:04 +0100

    Almost there, now support tunnels with corners

--- Day20/RaceCondition/main.py
+++ Day20/RaceCondition/main.py
@@ -66,86 +66,35 @@ def find_cheat_path_length(allowed_cheats=2, min_time_save=0):
 
     for candidate in path:
         for dx, dy in directions:
-            neighbor = Coordinate(candidate.x + dx, candidate.y + dy)
-            if grid[neighbor.x][neighbor.y] != "#":
-                continue
 
-            skip_one_wall = Coordinate(candidate.x + 2 * dx, candidate.y + 2 * dy)
-            skip_two_walls = Coordinate(candidate.x + 3 * dx, candidate.y + 3 * dy)
-            if skip_one_wall in path:
-                length = (
-                    path_lengths_from_start[candidate]
-                    + path_lengths_to_end[skip_one_wall]
-                    + 2
-                )
-                lengths.append(length)
-                time_saved = normal_length - length
-                if time_saved > 0:
-                    times_saved.append(time_saved)
-            elif skip_two_walls in path and grid[neighbor.x][neighbor.y] == "#":
-                length = (
-                    path_lengths_from_start[candidate]
-                    + path_lengths_to_end[skip_two_walls]
-                    + 3
-                )
-                lengths.append(length)
-                time_saved = normal_length - length
-                if time_saved > 0:
-                    times_saved.append(time_saved)
-
-    return sum(1 for s in times_saved if s >= min_time_save)
-
-
-def find_cheat_path_length2(allowed_cheats=2, min_time_save=0):
-    path_lengths_from_start, path_lengths_to_end = find_path_lengths()
-    lengths = []
-    times_saved = []
-    normal_length = path_lengths_from_start[find_coordinates(grid, "E")[0]]
-    print(normal_length)
-
-    for candidate in path:
-        for dx, dy in directions:
-            neighbor = Coordinate(candidate.x + dx, candidate.y + dy)
-            if grid[neighbor.x][neighbor.y] != "#":
-                continue
-
-            for skip_wall_number in range(1, allowed_cheats + 1):
-                tunnel = []
-                coord_after_cheat = Coordinate(
-                    candidate.x + (skip_wall_number) * dx,
-                    candidate.y + (skip_wall_number) * dy,
-                )
-
-                print("Before", candidate, "After", coord_after_cheat)
-                for t in range(1, skip_wall_number + 1):
-                    print(t)
-                    new_x = candidate.x + (1 + t) * dx
-                    new_y = candidate.y + (1 + t) * dy
-                    if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]):
-                        tunnel.append(grid[new_x][new_y])
-
-                print(
-                    coord_after_cheat,
-                    tunnel,
-                    all(entry == "#" for entry in tunnel),
-                    coord_after_cheat in path,
-                )
-                if not len(tunnel) == skip_wall_number:
+            tunnel = []
+            for cheat in range(1, allowed_cheats + 1):
+                newx = candidate.x + cheat * dx
+                newy = candidate.y + cheat * dy
+                neighbor = Coordinate(newx, newy)
+                if not (0 <= newx < len(grid) and 0 <= newy < len(grid[0])):
                     continue
 
-                if all(entry == "#" for entry in tunnel) and coord_after_cheat in path:
+                tunnel.append(grid[neighbor.x][neighbor.y])
+                if any(entry != "#" for entry in tunnel):
+                    continue
+
+                entry_after_tunnel = Coordinate(
+                    candidate.x + (cheat + 1) * dx, candidate.y + (cheat + 1) * dy
+                )
+                if entry_after_tunnel in path:
                     length = (
                         path_lengths_from_start[candidate]
-                        + path_lengths_to_end[coord_after_cheat]
+                        + path_lengths_to_end[entry_after_tunnel]
                         + len(tunnel)
+                        + 1
                     )
                     lengths.append(length)
                     time_saved = normal_length - length
-                    print("hello")
                     if time_saved > 0:
                         times_saved.append(time_saved)
 
-    print("Time saved", times_saved)
+    print("Time saved", sorted(times_saved))
     return sum(1 for s in times_saved if s >= min_time_save)
 
 
@@ -156,7 +105,8 @@ if __name__ == "__main__":
     end_coords = find_coordinates(grid, "E")[0]
     path = find_coordinates(grid, ".") + [start_coords, end_coords]
 
-    assert find_cheat_path_length2() == 44
+    assert find_cheat_path_length() == 44
+    assert find_cheat_path_length(20, 50) == 44
     grid = process_file("data.txt")
     start_coords = find_coordinates(grid, "S")[0]
     end_coords = find_coordinates(grid, "E")[0]
